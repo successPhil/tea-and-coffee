55f5fde391dcce9030394edede472246b32cd547
DLamarG <demondg8@gmail.com>
2023-11-02 18:18:12 -0500

    first commit/ workingBranch

--- backend/tea_api/models.py
+++ backend/tea_api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Tea(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
