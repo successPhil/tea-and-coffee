881bc9459e0e73371209f4d590f764377f0b88ca
DLamarG <demondg8@gmail.com>
2023-11-06 22:30:56 -0600

    added code on backend to fetch all tea categories and category types

--- backend/tea_api/urls.py
+++ backend/tea_api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('teas', views.TeaList.as_view(), name='tea-list'),
+    path('tea-categories', views.TeaCategory.as_view(), name='tea-categories'),
 ]
--- backend/tea_api/views.py
+++ backend/tea_api/views.py
@@ -4,13 +4,46 @@ from rest_framework import status
 from .models import Tea
 from .serializers import TeaSerializer
 import requests
+from rest_framework.response import Response
+from rest_framework import status
+
+
 
 class TeaList(APIView):
     queryset = Tea.objects.all()
     serializer_class = TeaSerializer
 
     def get(self, request):
-        # Fetch data from the API and update the database
+        
         response = requests.get('https://boonakitea.cyclic.app/api/teas')
         data = response.json()
-        return Response(data, status=status.HTTP_200_OK)
+
+        teasList = []
+
+        for entry in data:
+            if 'slug' in entry:
+                teasList.append(entry['slug'])
+            if 'types' in entry:
+                for tea_type in entry['types']:
+                    if 'slug' in entry['types'][tea_type]:
+                        teasList.append(entry['types'][tea_type]['slug'])
+
+        tea_data = {}
+
+        for tea_slug in teasList:
+            tea_response = requests.get(f'https://boonakitea.cyclic.app/api/teas/{tea_slug}')
+            if tea_response.status_code == 200:
+                tea_data[tea_slug] = tea_response.json()
+
+        return Response(tea_data, status=status.HTTP_200_OK)
+
+
+class TeaCategory(APIView):
+    
+    def get(self, request):
+        response = requests.get('https://boonakitea.cyclic.app/api/teas')
+        data = response.json()
+        
+        for slug in data:
+            if slug:
+              print(slug.slug)
