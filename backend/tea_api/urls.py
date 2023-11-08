ac4b08cdfd223e9cd0281450950bb3f05754fd8f
DLamarG <demondg8@gmail.com>
2023-11-07 18:43:25 -0600

    creatd views for each tea type

--- backend/tea_api/urls.py
+++ backend/tea_api/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('teas', views.TeaList.as_view(), name='tea-list'),
-    path('tea-categories', views.TeaCategory.as_view(), name='tea-categories'),
+    path('black-tea', views.BlackTeas.as_view(), name='black-tea-list'),
+    path('white-tea', views.WhiteTeas.as_view(), name='white-tea-list'),
+    path('earlgrey-tea', views.EarlGreyTeas.as_view(), name='earl-grey-tea-list'),
+    path('woolong-tea', views.WoolongTeas.as_view(), name='woolong-tea-list'),
+    path('green-tea', views.GreenTeas.as_view(), name='tea-categories'),
+    path('all-teas', views.AllTeas.as_view(), name='tea-categories'),
 ]
--- backend/tea_api/views.py
+++ backend/tea_api/views.py
@@ -9,41 +9,129 @@ from rest_framework import status
 
 
 
-class TeaList(APIView):
-    queryset = Tea.objects.all()
-    serializer_class = TeaSerializer
-
+class BlackTeas(APIView):
+    
     def get(self, request):
         
         response = requests.get('https://boonakitea.cyclic.app/api/teas')
         data = response.json()
+        # teasList = []
 
-        teasList = []
+        # for entry in data:
+        #     if 'slug' in entry and entry['slug'] == 'black':
+        #         teasList.append(entry['slug'])
 
-        for entry in data:
-            if 'slug' in entry:
-                teasList.append(entry['slug'])
-            if 'types' in entry:
-                for tea_type in entry['types']:
-                    if 'slug' in entry['types'][tea_type]:
-                        teasList.append(entry['types'][tea_type]['slug'])
+        # tea_data = {}
 
-        tea_data = {}
+        # for tea_slug in teasList:
+        #     tea_response = requests.get(f'https://boonakitea.cyclic.app/api/teas/{tea_slug}')
+        #     if tea_response.status_code == 200:
+        #         tea_data[tea_slug] = tea_response.json()
 
-        for tea_slug in teasList:
-            tea_response = requests.get(f'https://boonakitea.cyclic.app/api/teas/{tea_slug}')
-            if tea_response.status_code == 200:
-                tea_data[tea_slug] = tea_response.json()
+        return Response(data[0], status=status.HTTP_200_OK)
+    
 
-        return Response(tea_data, status=status.HTTP_200_OK)
+class WhiteTeas(APIView):
+    
+    def get(self, request):
+        
+        response = requests.get('https://boonakitea.cyclic.app/api/teas')
+        data = response.json()
+        # teasList = []
+
+        # for entry in data:
+        #     if 'slug' in entry and entry['slug'] == 'white':
+        #         teasList.append(entry['slug'])
+
+        # tea_data = {}
+
+        # for tea_slug in teasList:
+        #     tea_response = requests.get(f'https://boonakitea.cyclic.app/api/teas/{tea_slug}')
+        #     if tea_response.status_code == 200:
+        #         tea_data[tea_slug] = tea_response.json()
+
+        return Response(data[1], status=status.HTTP_200_OK)
+    
 
 
-class TeaCategory(APIView):
+class EarlGreyTeas(APIView):
+    
+    def get(self, request):
+        
+        response = requests.get('https://boonakitea.cyclic.app/api/teas')
+        data = response.json()
+        # teasList = []
+
+        # for entry in data:
+        #     if 'slug' in entry:
+        #             teasList.append(entry['slug'])
+        #     if 'types' in entry['slug']:
+        #         for tea_type in entry['types']:
+        #             if 'slug' in entry['types'][tea_type]:
+        #                 teasList.append(entry['types'])
+
+        # tea_data = {}
+
+        # for tea_slug in teasList:
+        #     tea_response = requests.get(f'https://boonakitea.cyclic.app/api/teas/{tea_slug}')
+        #     if tea_response.status_code == 200:
+        #         tea_data[tea_slug] = tea_response.json()
+
+        return Response(data[2], status=status.HTTP_200_OK)
+    
+
+
+class WoolongTeas(APIView):
+    
+    def get(self, request):
+        
+        response = requests.get('https://boonakitea.cyclic.app/api/teas')
+        data = response.json()
+        # teasList = []
+
+        # for entry in data:
+        #     if 'slug' in entry and entry['slug'] == 'oolong':
+        #         teasList.append(entry['slug'])
+
+        # tea_data = {}
+
+        # for tea_slug in teasList:
+        #     tea_response = requests.get(f'https://boonakitea.cyclic.app/api/teas/{tea_slug}')
+        #     if tea_response.status_code == 200:
+        #         tea_data[tea_slug] = tea_response.json()
+
+        return Response(data[4], status=status.HTTP_200_OK)
+    
+
+
+
+class GreenTeas(APIView):
+    
+    def get(self, request):
+        
+        response = requests.get('https://boonakitea.cyclic.app/api/teas')
+        data = response.json()
+        # teasList = []
+
+        # for entry in data:
+        #     if 'slug' in entry and entry['slug'] == 'green':
+        #         teasList.append(entry['slug'])
+
+        # tea_data = {}
+
+        # for tea_slug in teasList:
+        #     tea_response = requests.get(f'https://boonakitea.cyclic.app/api/teas/{tea_slug}')
+        #     if tea_response.status_code == 200:
+        #         tea_data[tea_slug] = tea_response.json()
+
+        return Response(data[5], status=status.HTTP_200_OK)
+
+
+
+class AllTeas(APIView):
     
     def get(self, request):
         response = requests.get('https://boonakitea.cyclic.app/api/teas')
         data = response.json()
-        
-        for slug in data:
-            if slug:
-              print(slug.slug)
+
+        return Response(data, status=status.HTTP_200_OK)
