b9de8770af7db3e7e712d02aa07e6e29beb2819b
successPhil <129565748+successPhil@users.noreply.github.com>
2023-11-18 16:15:48 -0500

    edited production scripts (#12)

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -11,11 +11,13 @@ export POSTGRES_DB=$3
 export POSTGRES_USER=$4
 export POSTGRES_PASSWORD=$5
 export NEW_VERSION=$6
+export AWS_ACCESS_KEY_ID=$7
+export AWS_SECRET_ACCESS_KEY=$8
 
 docker-compose -f docker-compose.prod.yml build --no-cache
 docker-compose -f docker-compose.prod.yml up -d
 
 # make sure the postgres container is ready, then run migrations
 sleep 10 
-docker exec tea-and-coffee-prod-api-1 python /src/manage.py makemigrations 
-docker exec tea-and-coffee-prod-api-1 python /src/manage.py migrate
+docker exec tea-and-coffee-api-1 python /src/manage.py makemigrations 
+docker exec tea-and-coffee-api-1 python /src/manage.py migrate
