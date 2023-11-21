020c6196a7f06f37f78acc9bb5098ebf94bb880e
itsEze <tylermkendall@gmail.com>
2023-11-20 21:41:07 -0600

    added functionality for see reviews. Needs plugged into the backed.

--- frontend/src/pages/Coffee.jsx
+++ frontend/src/pages/Coffee.jsx
@@ -1,12 +1,13 @@
 import { coffeeFetch } from "../api/dataApi"
 import { useEffect, useState } from "react"
 import Rating from '@mui/material/Rating';
-
+import SeeReview from '../features/Coffee/SeeReview'
 import AddCoffee from "../features/Coffee/AddCoffee"
 
 export default function Coffee() {
     const [ coffees, setCoffees ] = useState([])
     const [ addCoffeeForm, setAddCoffeeForm ] = useState(false)
+    const [ openReviewIndex, setOpenReviewIndex] = useState (null)
 
     const handleAddCoffee = () => {
         setAddCoffeeForm(true)
@@ -23,25 +24,36 @@ export default function Coffee() {
           .join(" ");
       };
 
+    const handleViewReviews = (index) => {
+        if (index == openReviewIndex) {
+            setOpenReviewIndex(null)
+        } else {
+            setOpenReviewIndex(index)
+        }
+    }
+
     const createCoffeeList = () => {
-        return coffees.map( coffee => {
+        return coffees.map( (coffee, index) => {
             return (
-                <div key={capitalizeWords(coffee.name)} className="coffee-card">
-                    <div className="coffee-header-image">
-                        <h1>{capitalizeWords(coffee.name)}</h1>
-                        <img className="coffee-thumbnail" src={coffee.picture}/>
-                    </div>
-                    <div className="coffee-interactive-info">
-                        <p>{coffee.description}</p>
-                        <div className="interactive-footer">
-                            <h4>{coffee.rating}<Rating name="read-only" value={coffee.rating} readOnly/></h4>
-                            <h4>Caffeine - {coffee.caffeine}mg/Serving</h4>
-                            <h4>Recipe</h4>
-                            <h4>See Reviews</h4>
-                            <h4> + Add Review</h4>
+                <>
+                    <div key={index} className="coffee-card">
+                        <div className="coffee-header-image">
+                            <h1>{capitalizeWords(coffee.name)}</h1>
+                            <img className="coffee-thumbnail" src={coffee.picture}/>
+                        </div>
+                        <div className="coffee-interactive-info">
+                            <p>{coffee.description}</p>
+                            <div className="interactive-footer">
+                                <h4>{coffee.rating}<Rating name="read-only" value={coffee.rating} readOnly/></h4>
+                                <h4>Caffeine - {coffee.caffeine}mg/Serving</h4>
+                                <h4>Recipe</h4>
+                                <h4 onClick={() => handleViewReviews(index)}> {index == openReviewIndex ? 'Close Reviews' : 'See Reviews'}</h4>
+                                <h4> + Add Review</h4>
+                            </div>
                         </div>
                     </div>
-                </div>
+                {openReviewIndex == index && (<SeeReview coffee={coffees[openReviewIndex]}/>)}
+                </>
                 )
             })
         }
