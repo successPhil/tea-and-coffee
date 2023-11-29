cfcd79c9d1ad9598febb3ad28949d07b751ed96c
successPhil <129565748+successPhil@users.noreply.github.com>
2023-11-28 20:45:30 -0500

    Review branch3 (#22)
    * added review front and and backend interface for addReview
    
    * added like feature to reviews, added favorites feature to Users
    
    ---------
    
    Co-authored-by: ItsEze <133399883+ItsEze@users.noreply.github.com>

--- backend/coffee_reviews/urls.py
+++ backend/coffee_reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from coffee_reviews.views import CoffeeReview
+from coffee_reviews.views import CoffeeReview, CoffeeLikes
 
 urlpatterns = [
 path('', CoffeeReview.as_view()),
+path('all', CoffeeLikes.as_view()),
 ]
---
+++ backend/users/user_serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from coffee_reviews.serializers import ReviewSerializer
+from .models import Users, Coffee
+
+class CoffeeSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Coffee
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    favorites = CoffeeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Users
+        fields = ["id", "user", "favorites"]
